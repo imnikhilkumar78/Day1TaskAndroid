7abf1987a73bb4d0e19eb6bf748339815cad9d89
unknown <Administrator@KANINI-LTP-471.gisbizdev.gisbiz.chennai.com>
2021-12-01 23:17:18 +0530

    Committed Complete Work

---
+++ app/src/main/java/com/example/textviewtask/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.textviewtask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val buttonCountDisplayView = findViewById<TextView>(R.id.buttonName)
+        val clickMeButton1 = findViewById<TextView>(R.id.button1)
+        val clickMeButton2 = findViewById<TextView>(R.id.button2)
+        val clickMeButton3 = findViewById<TextView>(R.id.button3)
+
+
+        clickMeButton1.setOnClickListener {
+
+            buttonCountDisplayView.setText("Button1 Clicked")
+        }
+        clickMeButton2.setOnClickListener {
+
+            buttonCountDisplayView.setText("Button2 Clicked")
+        }
+        clickMeButton3.setOnClickListener {
+
+            buttonCountDisplayView.setText("Button3 Clicked")
+        }
+    }
+}
